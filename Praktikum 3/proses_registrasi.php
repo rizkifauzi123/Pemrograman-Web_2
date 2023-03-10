ac653807fbff9a022a87376fa0b77ff103adde92
rizkifauzi123 <rizkiahmadfauzi1215@gmail.com>
2023-03-10 15:47:04 +0700

    tugas_3

---
+++ Praktikum 3/proses_registrasi.php
@@ -0,0 +1,20 @@
+<?php
+$domisili = ["Jakarta", "Bogor", "Depok", "Tanggerang"];
+
+$program_studi = [
+    "SI"=>"Sistem Informasi",
+    "TI"=>"Teknik Informatika",
+    "BD"=>"Bisnis Digital"
+];
+$skills = [
+    "HTML"=>10,
+    "CSS"=>10,
+    "Javascript"=>20,
+    "RWD Bootstrap"=>20,
+    "PHP"=>30,
+    "python"=>30,
+    "Java"=>50
+];
+
+
+?>
